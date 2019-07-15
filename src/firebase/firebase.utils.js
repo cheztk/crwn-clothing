5031a28cf85922f9625f5b1443f6de3f6f557095
Chezt <ckengchon@gmail.com>
2019-07-15 18:17:54 +0700

    finish sign-in handleSubmit

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -31,7 +31,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
       console.log("error creating user", error.message);
     }
   }
-  console.log(userRef);
+  //console.log(userRef);
 
   return userRef;
 };
