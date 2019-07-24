f25da0d063973a7302b5cc149301ce4cf2ed42f9
Chezt <ckengchon@gmail.com>
2019-07-24 17:02:22 +0700

    stripe integration,update favicon, title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_u5xlK6DoIUVtmgZDVO2HWbSI00tL0olxYL";
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN CLOTHING LTD"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+export default StripeCheckoutButton;
